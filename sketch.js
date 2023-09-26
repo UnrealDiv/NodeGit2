2280ae43a38f2a9ef240b742207d09691c734ee0
div11R <divyanshudw64@gmail.com>
2023-09-26 08:32:27 +0530

    first commit

---
+++ sketch.js
@@ -0,0 +1,14 @@
+const sizes = {
+  width:window.innerWidth,
+  height:window.innerHeight
+}
+
+function setup() {
+  createCanvas(sizes.width, sizes.innerHeight);
+}
+
+function draw() {
+  background(0);
+  fill(255);
+  ellipse(10,10,10);
+}
